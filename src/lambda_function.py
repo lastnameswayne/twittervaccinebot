e7e8488f3b849220a7a5f19ea30f08d1b02392f9
lastnameswayne <tiarnan1109@gmail.com>
2021-01-05 18:36:11 +0100

    try again

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,30 +1,19 @@
 import os
-import random
-import json
 from pathlib import Path
 import tweepy
 import csv
-
-ROOT = Path(__file__).resolve().parents[0]
-
-
-def get_tweet(tweets_file, excluded_tweets=None):
-    """Get tweet to post from CSV file"""
-
-    with open(tweets_file) as csvfile:
-        reader = csv.DictReader(csvfile)
-        possible_tweets = [row["tweet"] for row in reader]
-
-    if excluded_tweets:
-        recent_tweets = [status_object.text for status_object in excluded_tweets]
-        possible_tweets = [tweet for tweet in possible_tweets if tweet not in recent_tweets]
-
-    selected_tweet = random.choice(possible_tweets)
-
-    return selected_tweet
-
+import numpy as np
+import pandas as pd
+import io
 
 def lambda_handler(event, context):
+    url = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv'
+    df = pd.read_csv(url, usecols=['total_vaccinations', 'total_vaccinations_per_hundred'])
+    output = df.tail(1)
+    total_vaxx = output.iat[0,0].astype(int).astype(str)
+    total_perc=output.iat[0,1].astype(str)
+    tweet = "✅"+ total_vaxx +" vaccinations have been issued, " +total_perc +"% of the planet's population! ✅"
+
     print("Get credentials")
     consumer_key = os.getenv("CONSUMER_KEY")
     consumer_secret = os.getenv("CONSUMER_SECRET")
@@ -36,12 +25,7 @@ def lambda_handler(event, context):
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
 
-    print("Get tweet from csv file")
-    tweets_file = ROOT / "tweets.csv"
-    recent_tweets = api.user_timeline()[:3]
-    tweet = get_tweet(tweets_file)
-
-    print(f"Post tweet: {tweet}")
+    print("Post tweet:"+ tweet)
     api.update_status(tweet)
 
     return {"statusCode": 200, "tweet": tweet}
